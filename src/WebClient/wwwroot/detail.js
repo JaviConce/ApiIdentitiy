583b22e46d295fe48caa3b535b35f7802cd75265
JoseChris2911 <josechristianjerez@gmail.com>
2020-06-15 21:38:00 +0200

    botones funcionan

---
+++ src/WebClient/wwwroot/detail.js
@@ -0,0 +1,131 @@
+﻿cargarDetail();
+$('#btnEditar').click(function (e) {
+    console.log("boton pulsado");
+    let id = $(this).val();
+    console.log("id cogida");
+    editar(id);
+});
+$('#btnEliminar').click(function (e) {
+    console.log("boton pulsado");
+    let id = $(this).val();
+    console.log("id cogida");
+    borrar(id);
+});
+$('#btnAgregar').click(function (e) {
+    agregarTarea();
+});
+
+function cargarDetail() {
+    let datos;
+    if (localStorage.getItem("tareas")) {
+        let json = localStorage.getItem("tareas");
+        datos = JSON.parse(json);
+    }
+    
+    console.log(datos);
+    let thisresponse = document.querySelector('#response');
+    thisresponse.innerHTML = '';
+    for (let item of datos) {
+        console.log(item);
+        thisresponse.innerHTML += `
+        
+        <div class="card container mt-5">
+        
+        <div class="card-body ">
+            
+            <div class="row">
+                <div class="col-9">
+                    <b >Estado nombre: </b><p>${item.estadoNombre}</p>
+                    <b >Nombre: </b><p>${item.nombre}</p>
+                    <b >Responsable: </b><p>${item.responsableNombre}</p>
+                    <b >Tipo: </b><p>${item.tipoNombre}</p> 
+                    <b >PBI: </b><p>${item.pbiNombre}</p> 
+                </div>
+                <div class="col-3">
+                    <button class=" btn btn-warning" type="button" id="btnEditar" value="${item.id}" style="background-color: orange;">Editar</button>
+                    <button class=" btn btn-danger" type="button" id="btnEliminar" value="${item.id}" style="background-color: red;">Eliminar</button>
+                </div>
+            </div>
+        </div>
+    </div> `
+    }
+}
+function agregarTarea() {
+    let urlAdd = 'https://localhost:6001/api/orden';
+    
+    
+    var tarea = {EstadoNombre: $('#txtEstadonombre').val(), Nombre: $('#txtNombre').val(), PBINombre: $('#txtPBINombre').val(), ResponsableNombre: $('#txtResponsablenombre').val(), TipoNombre: $('#txtTiponombre').val() };
+    console.log(JSON.stringify(tarea));
+    console.log(localStorage.getItem("token"));
+    var settings = {
+        "url": urlAdd,
+        "method": "POST",
+        "timeout": 0,
+        //"processData": false,
+       // "mimeType": "multipart/form-data",
+        "contentType": "application/json",
+        "data": JSON.stringify(tarea),
+        "headers": {
+            "Authorization": "Bearer " + localStorage.getItem("token"),
+            
+        },
+    };
+    //console.log(form);
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+        $('#modalAgregar').modal('hide');
+    }).fail(function () {
+        alert("No se pudo realizar la accion");
+    });
+    
+}
+
+/*function editar(id) {
+    //alert("esta es la id = " + id);
+    let urlDelete = 'https://localhost:6001/api/orden/delete';
+    var settings = {
+        "url": urlDelete,
+        "method": "DELETE",
+        "timeout": 0,
+        //"processData": false,
+        // "mimeType": "multipart/form-data",
+        "dataType": "int",
+        "data": id,
+        "headers": {
+            "Authorization": "Bearer " + localStorage.getItem("token"),
+
+        },
+    };
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+        //$('#modalAgregar').modal('hide');
+        alert("Se borro de la base de datos");
+    }).fail(function () {
+        alert("No se pudo realizar la accion");
+    });
+}*/
+function borrar(id) {
+    let urlDelete = 'https://localhost:6001/api/orden/delete/'+id;
+    var settings = {
+        "url": urlDelete,
+        "method": "DELETE",
+        "timeout": 0,
+        //"processData": false,
+        // "mimeType": "multipart/form-data",
+        //"dataType": "int",
+        //"data": id,
+        "headers": {
+            "Authorization": "Bearer " + localStorage.getItem("token"),
+
+        },
+    };
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+        //$('#modalAgregar').modal('hide');
+        alert("Se borro de la base de datos");
+    }).fail(function () {
+        alert("No se pudo realizar la accion");
+    });
+
+
+}
--- src/WebClient/wwwroot/index.js
+++ src/WebClient/wwwroot/index.js
@@ -1,38 +1,39 @@
-$('#btnlogin').click(function (e){
-    login();
-});
-$('#btngetall').click(function (e) {
-    getall();
-});
-$('.btn-editar').click(function (e) {
-    console.log("boton pulsado");
-    let id = $(this).val();
-    console.log("id cogida");
-    editar(id);
-});
-$('#btngettoken').click(function (e) {
-    var form = new FormData();
-    form.append("grant_type", "password");
-    form.append("scopes", "api1");
-    form.append("client_id", "api1");
-    form.append("client_secret", "secret");
-    form.append("UserName", "operador1");
-    form.append("password", "123");
 
-    var settings = {
-        "url": "https://localhost:5001/connect/token",
-        "method": "POST",
-        "timeout": 0,
-        "processData": false,
-        "mimeType": "multipart/form-data",
-        "contentType": false,
-        "data": form
-    };
-
-    $.ajax(settings).done(function (response) {
-        alert(response);
+    $('#btnlogin').click(function (e) {
+        login();
     });
-});
+    $('#btngetall').click(function (e) {
+        getall();
+    });
+    $('#btngettoken').click(function (e) {
+        var form = new FormData();
+        form.append("grant_type", "password");
+        form.append("scopes", "api1");
+        form.append("client_id", "api1");
+        form.append("client_secret", "secret");
+        form.append("UserName", "operador1");
+        form.append("password", "123");
+
+        var settings = {
+            "url": "https://localhost:5001/connect/token",
+            "method": "POST",
+            "timeout": 0,
+            "processData": false,
+            "mimeType": "multipart/form-data",
+            "contentType": false,
+            "data": form
+        };
+
+        $.ajax(settings).done(function (response) {
+            alert(response);
+            console.log(response);
+        });
+    });
+
+
+
+
+
 
 function editar(id) {
     alert("esta es la id = " + id);
@@ -64,57 +65,15 @@ function login() {
         var json = JSON.parse(response);
         var bearer = json["access_token"];
         console.log(bearer);
-
+        localStorage.setItem("token", bearer);
         obtenerOrdenes(bearer);
 
-
+        console.log(bearer);
     }).fail(function () {
         alert("Este usuario no existe");
     });
 
-    /*$.ajax(settings).done(function (response) {
-
-        url: urlPedirOrden,
-        type: 'POST',
-        data: response,
-        dataType: 'json',
-        contentType: false,
-        success: function (data) {
-               let datos = JSON.parse(data.d)
-
-               let thisresponse = document.querySelector('#response')
-               thisresponse.innerHTML = '';
-              for (let item of datos) {
-                                    thisresponse.innerHTML += ` 
-               <div class="card container mt-5">
-        
-        <div class="card-body ">
-            
-            <div class="row">
-                <div class="col-9">
-                    <b >Nombre: </b><p>${item.Nombre}</p>
-                    <b >Responsable: </b><p>${item.ResponsableNombre}</p>
-                    <b >Tipo: </b><p>${item.TipoNombre}</p> 
-                    <b >PBI: </b><p>${item.PBINombre}</p> 
-                </div>
-                <div class="col-3">
-                    <button id="btnEditar" style="background-color: orange;">Editar</button>
-                    <button id="btnEliminar" style="background-color: red;">Eliminar</button>
-                </div>
-            </div>
-        </div>
-    </div> `
-
-
-              }
-              window.location = "detail.html";
-
-
-        },
-        error: function(xhr) {
-            console.error('Error: ', xhr);
-        }
-    });*/
+    
 }
 function obtenerOrdenes(token) {
     var settings = {
@@ -128,12 +87,16 @@ function obtenerOrdenes(token) {
     };
     console.log(settings);
     $.ajax(settings).done(function (response) {
-        cargarDetail(response);
-        //window.location = "detail.html";
+        //cargarDetail(response);
+        let json = JSON.stringify(response);
+        console.log(json);
+        localStorage.setItem("tareas", json);
+        window.location = "detail.html";
+
     });
 }
-function cargarDetail(datos) {
-    
+function cargarDetail() {
+    let datos = localStorage.getItem("tareas");
     let thisresponse = document.querySelector('#response');
     thisresponse.innerHTML = '';
     for (let item of datos) {
@@ -153,7 +116,7 @@ function cargarDetail(datos) {
                     <b >PBI: </b><p>${item.pbiNombre}</p> 
                 </div>
                 <div class="col-3">
-                    <button class=" btn btn-warning btn-editar" type="button" id="btnEditar" value="${item.id}" style="background-color: orange;">Editar</button>
+                    <button class=" btn btn-warning" type="button" id="btnEditar" value="${item.id}" style="background-color: orange;">Editar</button>
                     <button class=" btn btn-danger" type="button" id="btnEliminar" value="${item.id}" style="background-color: red;">Eliminar</button>
                 </div>
             </div>
@@ -162,41 +125,9 @@ function cargarDetail(datos) {
     }
 }
 
-function getall() {
-    let urlPrueba = 'http://localhost:6001/api/orden';
-    let usuario = $('#txtUsuario').val();
-    let contraseña = $('#txtPass').val();
-    $.ajax({
-        url: urlPrueba,
-        type: 'POST',
-        data: JSON.stringify({
-            ClientId: usuario,
-            ClientSecret: contraseña
-        }),
-        dataType: 'json',
-        contentType: "application/json; charset=utf-8",
-        success: function (data) {
-            let respuestaServicioWeb = JSON.parse(data.d);
-            $('#resultado').text(respuestaServicioWeb);
-            if (respuestaServicioWeb = 'true') {
-                window.location = "detail.html";
-            }
-
-        },
-        error: function (xhr) {
-            console.error('Error: ', xhr);
-        }
-
-    });
-}
 
 
 function logout() {
     
 }
 
-
-function mostrarid(id){
-    alert("Su ID es: "+id)
-
-}
